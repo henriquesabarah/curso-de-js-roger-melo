57adda18726d3986d0646f6ca94902471f15cf4f
henriquesabarah <henriquesabarah@gmail.com>
2021-07-04 23:58:55 -0300

    Exercício 15

--- exercicio-15/app.js
+++ exercicio-15/app.js
@@ -5,32 +5,37 @@
     uma classe 'video';
   - Exiba no console os elementos filhos da ul com a classe já inserida.
 */
+const ul = document.querySelector('.videos')
+const lis = Array.from(ul.children)
 
+const insetVideoClass = li => {
+  li.classList.add('video')}
 
-
+lis.forEach(insetVideoClass)
+console.log(lis)
 /*
   02
 
   - Usando a propriedade adequada, descubra quem é o elemento pai do h2
     e exiba-o no console;
 */
-
-
+const h2 = document.querySelector('h2')
+console.log(h2.parentElement)
 
 /*
   03
 
   - Descubra quem é o próximo elemento irmão do h1 e exiba-o no console;
 */
-
-
+const h1 = document.querySelector('h1')
+console.log(h1.nextElementSibling)
 
 /*
   04
 
   - Descubra quem é o irmão anterior da ul e exiba-o no console;
 */
-
+console.log(ul.previousElementSibling)
 
 
 /*
@@ -39,9 +44,15 @@
   - Quando um clique acontecer em alguma das lis, faça com que a li clicada seja  
     exibida no console.
 */
+const showclickLi = event => {
+  console.log(event.target)
+}
 
+const addClickEvent = li => {
+  li.addEventListener('click', showclickLi )
+}
 
-
+lis.forEach(addClickEvent)
 /*
   06
 
@@ -60,9 +71,24 @@ const videos = [{
   length: '00:02:55'
 }]
 
+const handleClickButton = () => {
+  console.log('clicou no botão')
+  videos.forEach(video => {
+    ul.innerHTML += `<li>${video.name}</li>`
+  })
+}
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', handleClickButton)
+
 /*
   07
 
   - Se um clique no h1 acontecer, faça com que todos os elementos dentro do body 
     sejam removidos.
 */
+const body = document.querySelector('body')
+h1.addEventListener('click', () => {
+  body.innerHTML = ''
+})
